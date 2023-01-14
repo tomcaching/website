d42e27fdd2a9d1bc06abcb100720d7bf5f0606ef
Jiří Vrba <jiri@vrba.dev>
2023-01-14 11:56:17 +0100

    Add geocaching colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        geocaching: {
+          green: "#02874d",
+          white: "#ffffff",
+          brown: {
+            darker: "#5f452a",
+            dark: "#83603f",
+            light: "#f8edd4"
+          }
+        }
+      }
+    },
   },
   plugins: [],
 };
