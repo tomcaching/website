1e0a4d794ccad06a835f4819ac0ca3ed0bca0348
Jiří Vrba <jiri@vrba.dev>
2023-01-17 19:48:56 +0100

    Add methods for API calls

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,23 @@ import { type Cache } from "@/types";
 
 const api = "https://api.tomcaching.fun";
 
-export const fetchCaches = async (): Promise<Array<Cache>>  => {
+export const fetchCaches = async (): Promise<Array<Cache>> => {
     return await fetch(`${api}/api/caches`).then(response => response.json()) as Array<Cache>;
+};
+
+export const unlockCache = async (id: number, solution: string): Promise<Array<Cache>> => {
+    const payload = {
+        method: "post",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ solution })
+    };
+
+    return await fetch(`${api}/api/caches/unlock/${id}`, payload).then(response => response.json()) as Array<Cache>;
+};
+
+export const markCacheFound = async (id: number): Promise<Array<Cache>> => {
+    return await fetch(`${api}/api/caches/mark-found/${id}`, { method: "post" }).then(response => response.json()) as Array<Cache>;
 };
